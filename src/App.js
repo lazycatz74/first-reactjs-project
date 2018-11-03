9898157587d9bf7c015999dfef9a31a2b187480c
Anh Vuong <anhvvuong0212@gmail.com>
2018-11-03 16:00:13 +1100

    Fetch data with JS' fetch API, delete sample/ hardwired data
    -> Concat string with template literals
    -> Fetch data to change local state within the mounting process

--- src/App.js
+++ src/App.js
@@ -2,104 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 const creator = {
   firstName: 'Alex',
   lastName: 'Vuong'
 }
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
-
-const hobbies = [
-  {
-      id: '1',
-      title: 'Swimming',
-      rate: 9
-  },
-  {
-      id: '2',
-      title: 'Playing games',
-      rate: 4
-  },
-  {
-      id: '3',
-      title: 'Watching movies',
-      rate: 7.5
-  },
-  {
-      id: '4',
-      title: 'Laiding (just kidding)',
-      rate: 10
-  }
-]
-
-const todoList = [
-  {
-    id: '1',
-    title: 'Learn React',
-    estimateTime: '1 month'
-  },
-  {
-    id: '2',
-    title: 'Go on a trip to Great Ocean Road',
-    estimateTime: '2 days'
-  },
-  {
-    id: '3',
-    title: 'Travel to other state, e.g. Tasmania, Canberra & Queensland',
-    estimateTime: '5 - 6 days'
-  }
-]
-
-function TodoList(props, index) {
-  const listArray = props.list;
-  const listItems = listArray.map(item =>
-    <li key={item.id}>{item.title + ': ' + item.estimateTime}</li>
-  );
-  return (
-    <ul className="todo-list">
-      {listItems}
-    </ul>
-  );
-}
-
-const ReversedTodoList = (props, index) =>
-  <ul className="reversed-todo-list">
-    {
-      props.list.slice(0).reverse().map(item =>
-        <li key={props.list - item.id}>{item.estimateTime + ': ' + item.title}</li>
-      )
-    }
-  </ul>
-
 const isSearched = searchTerm => item =>
     //  Add condition
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
-/**
- * ES5
- function isSearched(searchTerm) {
-   return function(item) {
-     //  Add condition
-     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-   }
- }
- */
 
 class App extends Component {
   /**
@@ -112,24 +28,32 @@ class App extends Component {
     this.state = {
       appCreator: creator,
 
-      // list: list,
-      // hobbies: hobbies,
-      // todoList: todoList
-      // ---Using shorthand--->>
-      list,
-      hobbies,
-      searchTerm: '',
-      todoList,
-      uselessButton: <button>This button do nothing at all</button>
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
-    // About binding:
-    // https://medium.freecodecamp.org/this-is-why-we-need-to-bind-event-handlers-in-class-components-in-react-f7ea1a6f93eb
-    this.onDismiss = this.onDismiss.bind(this);
+    //  Bind methods here
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    console.log('Perfom searching and fetching form API');
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onDismiss(id) {
+    console.log('Dismissing item with objectID: ' + id);
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
@@ -155,59 +79,32 @@ class App extends Component {
   */
 
   render() {
-    let appState = this.state; // make changes on the component's properties, not the variables defined outside of the component
-    let helloWorld = <h1>Welcome to Viet Chip Journal XD</h1>;
-    const { searchTerm, list } = this.state;
+    console.log('Render app');
+    const { appCreator, searchTerm, result } = this.state;
+
+    if (!result) { return null; }
 
     return (
       <div className="page">
         <div className="author-intro">
-          {helloWorld}
+          <h1>Welcome to Viet Chip Journal XD</h1>
           <h2>My name is:</h2>
-          <p>{appState.appCreator.firstName} {appState.appCreator.lastName}</p>
+          <p>{appCreator.firstName} {appCreator.lastName}</p>
         </div>
         {/* Search bar */}
         <div className="interactions">
           <Search
             value={searchTerm}
-            onChange={this.onSearchChange} // Happen immediately after the value is changed
+            onChange={this.onSearchChange}
           >
             Search bar:
           </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
-        <h3 style={{color:'green'}}>These are my hobbies:</h3>
-        <table className="hobbies-table">
-          <tbody>
-            <tr>
-              {/**
-                * Destructure list's properties here to assign table heading automatically
-                */}
-              <th>Hobby</th>
-              <th>Rate</th>
-            </tr>
-            {/**
-              * filter(() => item.title.includes(searchTerm))
-              * Problem: this = filter, filter.state = undefined
-              * Solution: pass in searchTerm as a param
-              */}
-            {appState.hobbies.filter(isSearched(searchTerm)).map(item => // Add filter here to perfom search
-              <tr key={item.id}>
-                <td>{item.title}</td>
-                <td>{item.rate}</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-        <h3 style={{color:'orange'}}>To-do list:</h3>
-        <TodoList list={appState.todoList} />
-        <h3 style={{color:'red', fontStyle:'italic', fontWeight:'900'}}>Reversed To-do list:</h3>
-        <ReversedTodoList list={appState.todoList} />
-        {appState.uselessButton}
       </div>
     );
   }
@@ -336,7 +233,6 @@ const Table = ({list, pattern, onDismiss}) => {
 }*/
 
 const Button = ({ onClick, className = '', children }) => {
-  console.log(className);
   return (
     <button
       onClick={onClick}
