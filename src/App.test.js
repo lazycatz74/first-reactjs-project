e490f1d5793d9d55e9df32690eb23ffb4251d269
Anh Vuong <anhvvuong0212@gmail.com>
2018-11-12 23:16:13 +1100

    Create component tests using Enzyme
    Enzyme has 3 rendering mechanisms: shallow(), mount(), and render()
    You can actually call functions and interacts with objects created by them -> testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App, { Search, Button, Table } from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
 
@@ -55,6 +59,16 @@ describe('Button', () => {
     expect(tree).toMatchSnapshot();
   })
 
+  let callCount = 0;
+
+  it('simultates click events', () => {
+    const element = mount(
+      <Button onClick={() => callCount++}>Click here</Button>
+    );
+    element.find('button').simulate('click');
+    expect(callCount).toBe(1);
+  });
+
 });
 
 describe('Table', () => {
@@ -79,5 +93,13 @@ describe('Table', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  });
 
 });
